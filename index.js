ae9a2303c224bf794eea573782115226cde48b6c
Yahonan Alcântara <110312787+Yahonan@users.noreply.github.com>
2026-03-01 16:51:24 -0300

    Refatora o gerenciamento de produtos e o manuseio de armazenamento local.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let total = 0;
 
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
 const preco = document.getElementById("precoProduto");
 
 const nome = document.getElementById("nomeProduto");
@@ -8,37 +10,80 @@ const valorTotal = document.getElementById("valorTotal");
 
 const listaProdutos = document.getElementById("listaProdutos");
 
-function adicionarProduto(){
+function limparCampos(inputNome, inputPreco){
+    inputNome.value = ''
+    inputPreco.value = ''
+}
 
-    const itemProduto = document.createElement('li');
+function salvarCarrinho(){
+    localStorage.setItem("carrinho", JSON.stringify(carrinho))
+}
 
+function retornarCarrinho(){
+        window.addEventListener("DOMContentLoaded", () => {
+            carrinho.forEach((produto) => {
+                criarItemNaTela(produto.nome, parseFloat(produto.preco));
+                atualizarSomaTotal(parseFloat(produto.preco));
+            })
+            salvarCarrinho();
+        });
+    }
+
+function atualizarSomaTotal(valorRecebido){
+    total = total + valorRecebido;
+    valorTotal.textContent = `Total R$ ${total.toFixed(2)}`;
+}
+
+function dadosSaoValidos(nomeRecebido, precoRecebido){
+    if(nomeRecebido.trim() === ''){
+        alert("O nome do produto não pode estar vazio e precisa ser um texto");
+        return false
+    }
+
+    if(precoRecebido.trim() <= 0 || isNaN(precoRecebido)){
+        alert("O preco não pode ser menor que 0 e precisa ser um número")
+        return false
+    }
+    return true; 
+}
+    //Mudança na lógica, seguindo os princípios do clean code e DRY 
+    //Tava feio uma função gigante fazendo tudo
+function adicionarProduto() {
     let valorProduto = parseFloat(preco.value);
 
-    if(nome.value.trim() === '' || preco.value.trim() === ''){
-      alert("Por favor, informe o nome e o preço do produto")
-    } else {
-        itemProduto.textContent = `${nome.value} - R$ ${preco.value}`
+    if (!dadosSaoValidos(nome.value, preco.value)) return;
 
-        const btnRemover = document.createElement('button');
-        btnRemover.textContent = '❌'
+    criarItemNaTela(nome.value, valorProduto);
 
-        btnRemover.onclick = function(){
-            removerProduto(itemProduto, valorProduto)
-        }
-        
-        listaProdutos.appendChild(itemProduto)
-        itemProduto.appendChild(btnRemover)
-        console.log(itemProduto)
-        console.log(btnRemover)
+    atualizarSomaTotal(valorProduto);
 
-        total = total + valorProduto;
+    carrinho.push({nome: nome.value, preco: valorProduto});
 
-        valorTotal.textContent = `Total R$ ${total.toFixed(2)}`;
-
-        nome.value = ''
-        preco.value = ''
-    }
+    salvarCarrinho();
+    limparCampos(nome, preco);
 }
+
+function criarItemNaTela(nomeRecebido, precoRecebido){
+
+    const criarItem = document.createElement('li')
+    criarItem.textContent = `${nomeRecebido} - R$ ${precoRecebido}`
+    const btnRemover = document.createElement('button');
+                
+    btnRemover.textContent = '❌'
+                 
+    btnRemover.onclick = function(){
+    removerProduto(criarItem, precoRecebido)
+    }
+
+    listaProdutos.appendChild(criarItem)
+    criarItem.appendChild(btnRemover)
+}
+
+preco.addEventListener("keydown", (evento) => {
+    if(evento.key === 'Enter'){
+        adicionarProduto()
+    }
+});
 
 function removerProduto(itemRemovivel, valorSubtraivel){
 
@@ -46,3 +91,5 @@ function removerProduto(itemRemovivel, valorSubtraivel){
     valorTotal.textContent = `Total: R$ ${total.toFixed(2)}`
     itemRemovivel.remove();
 }
+
+retornarCarrinho();
